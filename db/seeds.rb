ff12a0c13cd78dacba2d3073f194e3b4a76cc031
Yuezhi Li <clivialee@gmail.com>
2018-05-10 13:50:53 +0800

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Restaurant.create(
+    name: Faker::HitchhikersGuideToTheGalaxy.planet,
+    address: Faker::HitchhikersGuideToTheGalaxy.location,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+end
